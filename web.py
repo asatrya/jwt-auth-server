539fd635d111c146ba3a51a9fb843a6efec2dfde
Aditya Satrya <aditya.satrya@gmail.com>
2019-06-23 13:43:39 +0700

    Add handling authentication by checking whether request header has 'Bearer ' prefix before token

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ User.Objects.create(email='user@email.com', password='password')
 
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
-JWT_EXP_DELTA_SECONDS = 20
+JWT_EXP_DELTA_SECONDS = 3600
 
 
 def json_response(body='', **kwargs):
@@ -32,8 +32,8 @@ async def login(request):
         'user_id': user.id,
         'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
     }
-    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
-    return json_response({'token': jwt_token.decode('utf-8')})
+    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM).decode('utf-8')
+    return json_response({'token': jwt_token})
 
 
 async def get_user(request):
@@ -41,16 +41,23 @@ async def get_user(request):
 
 
 async def auth_middleware(app, handler):
+    def get_token(header):
+        if not header.startswith('Bearer '):
+            return header
+        else:
+            return header[len('Bearer '):]
+
     async def middleware(request):
         request.user = None
-        jwt_token = request.headers.get('authorization', None)
+        jwt_token = get_token(request.headers.get('authorization', None))
+        print('Token reveived=' + jwt_token)
         if jwt_token:
             try:
-                payload = jwt.decode(jwt_token, JWT_SECRET,
-                                     algorithms=[JWT_ALGORITHM])
-            except (jwt.DecodeError, jwt.ExpiredSignatureError):
-                return json_response({'message': 'Token is invalid'},
-                                     status=400)
+                payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+            except jwt.DecodeError as e:
+                return json_response({'message': 'Token is invalid: ' + str(e)}, status=400)
+            except jwt.ExpiredSignatureError:
+                return json_response({'message': 'Token is expired'}, status=400)
 
             request.user = User.Objects.get(id=payload['user_id'])
         return await handler(request)
