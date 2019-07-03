3dc12bf6b15cdf4fe7cdc2d14f3c5ee219b879c8
Aditya Satrya <aditya.satrya@gmail.com>
2019-07-03 22:18:38 +0700

    create simple JWT auth server

--- web.py
+++ web.py
@@ -1,12 +1,13 @@
 import json
 from datetime import datetime, timedelta
 
+import asyncio
 from aiohttp import web
 import jwt
 
 from models import User
 
-User.Objects.create(email='user@email.com', password='password')
+User.Objects.create(email='admin', password='password')
 
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
@@ -19,11 +20,13 @@ def json_response(body='', **kwargs):
     return web.Response(**kwargs)
 
 
+@asyncio.coroutine
 async def login(request):
-    post_data = await request.post()
+    post_data = await request.json()
 
     try:
-        user = User.Objects.get(email=post_data['email'])
+        API_KEY = request.headers.get('X-Api-Key', JWT_SECRET)
+        user = User.Objects.get(email=post_data['account'])
         user.match_password(post_data['password'])
     except (User.DoesNotExist, User.PasswordDoesNotMatch):
         return json_response({'message': 'Wrong credentials'}, status=400)
@@ -32,39 +35,11 @@ async def login(request):
         'user_id': user.id,
         'exp': datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS)
     }
-    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM).decode('utf-8')
-    return json_response({'token': jwt_token})
+
+    jwt_token = jwt.encode(payload, API_KEY, JWT_ALGORITHM).decode('utf-8')
+    res = json_response({'jwt': jwt_token})
+    return res
 
 
-async def get_user(request):
-    return json_response({'user': str(request.user)})
-
-
-async def auth_middleware(app, handler):
-    def get_token(header):
-        if not header.startswith('Bearer '):
-            return header
-        else:
-            return header[len('Bearer '):]
-
-    async def middleware(request):
-        request.user = None
-        jwt_token = get_token(request.headers.get('authorization', None))
-        print('Token reveived=' + jwt_token)
-        if jwt_token:
-            try:
-                payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-            except jwt.DecodeError as e:
-                return json_response({'message': 'Token is invalid: ' + str(e)}, status=400)
-            except jwt.ExpiredSignatureError:
-                return json_response({'message': 'Token is expired'}, status=400)
-
-            request.user = User.Objects.get(id=payload['user_id'])
-        return await handler(request)
-
-    return middleware
-
-
-app = web.Application(middlewares=[auth_middleware])
-app.router.add_route('GET', '/get-user', get_user)
+app = web.Application()
 app.router.add_route('POST', '/login', login)
